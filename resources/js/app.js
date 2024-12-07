893b997f01226a66bc7207d44fcafd79b0ba8875
Shinsei <murakami.shinsei.zn@gmail.com>
2024-12-07 15:59:57 +0900

    vite環境のLaravelにvueを一部導入

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,29 @@
-import './bootstrap';
+import "./bootstrap";
+import { createApp } from "vue";
+import ExampleComponent from "./components/ExampleComponent.vue";
+import AnotherComponent from "./components/AnotherComponent.vue";
+
+// コンポーネントのマッピング
+const components = [
+    {
+        id: "example-component",
+        component: ExampleComponent,
+    },
+    {
+        id: "another-component",
+        component: AnotherComponent,
+    },
+    // 新しいコンポーネントが必要な場合はここに追加
+];
+
+// コンポーネントを動的にマウントする関数
+const mountComponent = ({ id, component }) => {
+    const element = document.getElementById(id);
+    if (element) {
+        const app = createApp(component);
+        app.mount(`#${id}`);
+    }
+};
+
+// 各コンポーネントをループで処理
+components.forEach(mountComponent);
---
+++ resources/js/components/AnotherComponent.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>Another Component</h1>
+    </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+h1 {
+    color: red;
+}
+</style>
